a6077988fa91bbe541fc5f60b92632fbe1b6252c
AmirAllami <allamiamir@gmail.com>
2021-01-02 10:20:08 +0100

    add hashRouter to fix routing issue with gh pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
-import { Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Education from "./Components/Education";
 import Projects from "./Components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={About} />
-        <Route exact path="/Education" component={Education} />
-        <Route exact path="/Projects" component={Projects} />
-      </Switch>
-    </div>
+    <HashRouter basename="/">
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/Education" component={Education} />
+          <Route exact path="/Projects" component={Projects} />
+        </Switch>
+      </div>
+    </HashRouter>
   );
 }
 
